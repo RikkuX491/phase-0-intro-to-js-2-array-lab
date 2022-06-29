1896674ce0ccf88640290e00343b2d6e34db6504
Ricardo Guerra <RikkuG491@gmail.com>
2022-06-29 15:58:23 -0400

    commit tree 9b25c0fc51a0d4cccf4ac6bbfc9f31eae18dc324

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    return [...cats, name];
+}
+
+function prependCat(name){
+    return [name, ...cats];
+}
+
+function removeLastCat(){
+    let copyOfCats = [...cats];
+    copyOfCats.pop();
+    return copyOfCats;
+}
+
+function removeFirstCat(){
+    let copyOfCats = [...cats];
+    copyOfCats.shift();
+    return copyOfCats;
+}
